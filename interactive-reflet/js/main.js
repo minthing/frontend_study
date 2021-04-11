b4b1dcf3e227d1c7ad7d06effa73025cd880ddb5
minthing <minjeongandk@gmail.com>
2021-04-11 17:17:57 +0900

    js 이벤트 위임

--- interactive-reflet/js/main.js
+++ interactive-reflet/js/main.js
@@ -1,1 +1,23 @@
-console.log("main.js");
+(()=>{
+    const reflet = document.querySelector('.reflet');
+
+    function getTarget(elem, className){
+        while(!elem.classList.contains(className)){
+            elem = elem.parentNode;
+
+            if(elem.nodeName == 'BODY'){
+                elem = null;
+                return;
+            }
+        }
+
+        return elem;
+    }
+
+    reflet.addEventListener('click', e => {
+        let pageElem = getTarget(e.target, 'page');
+        if(pageElem){
+            pageElem.classList.add('page_flipped')
+        }
+    })
+})();
