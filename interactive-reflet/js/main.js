9522d2d6255069bc99afd1f74da6746d78af6489
minthing <minjeongandk@gmail.com>
2021-04-13 19:16:39 +0900

    닫기 동작

--- interactive-reflet/js/main.js
+++ interactive-reflet/js/main.js
@@ -1,5 +1,6 @@
 (()=>{
     const reflet = document.querySelector('.reflet');
+    const pageElems = document.querySelectorAll('.page');
     let count = 0;
 
     function getTarget(elem, className){
@@ -15,6 +16,13 @@
         return elem;
     }
 
+    function close(){
+        pageElems[2].classList.remove('page_flipped');
+        setTimeout(() => {
+            pageElems[0].classList.remove('page_flipped');
+        }, 650);
+    }
+
     reflet.addEventListener('click', e => {
         let pageElem = getTarget(e.target, 'page');
         if(pageElem){
@@ -23,6 +31,13 @@
         }
         if(count == 2){
             document.body.classList.add('reflet-opened');
+        }
+
+        let closeBtn = getTarget(e.target, 'close-btn');
+        if(closeBtn){
+            document.body.classList.remove('reflet-opened');
+            count = 0;
+            close();
         }
     })
 })();
